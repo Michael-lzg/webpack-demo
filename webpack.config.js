10d7586a859329cb280db8e1a9a9cf134e80aae7
lzg <michael_lizg@163.com>
2018-11-07 08:26:03 +0800

    插件的使用

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
-const path = require('path')
+const path = require('path');  // 路径处理模块
+const webpack = require('webpack');  // 这个插件不需要安装，是基于webpack的，需要引入webpack模块
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 引入HtmlWebpackPlugin插件
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // 引入CleanWebpackPlugin插件
+
 module.exports = {
   entry: path.join(__dirname, '/src/index.js'), // 入口文件
   output: {
@@ -9,7 +13,8 @@ module.exports = {
     contentBase: './dist', // 本地服务器所加载文件的目录
     port: '8089', // 设置端口号为8088
     inline: true, // 文件修改后实时刷新
-    historyApiFallback: true //不跳转
+    historyApiFallback: true, //不跳转
+    hot: true // 热更新
   },
   devtool: 'source-map', // 会生成对于调试的完整的.map文件，但同时也会减慢打包速度
   module: {
@@ -35,5 +40,13 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    new webpack.BannerPlugin('版权所有，翻版必究'), // new一个插件的实例
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, '/src/index.template.html') // new一个这个插件的实例，并传入相关的参数
+    }),
+    new CleanWebpackPlugin(['dist']),  // 所要清理的文件夹名称
+    new webpack.HotModuleReplacementPlugin() // 热更新插件 
+  ]
 }
